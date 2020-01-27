99a6f109ce5163d58c7174361826298bc0e49347
Roni <bibburi@hotmail.com>
2020-01-28 00:57:02 +0200

    socket.io käytössä myös palvelimella keskusteludatan välittämiseen

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const http = require('http')
 const app = require('./app')
 const config = require('./config')
+const Conversation = require('./models/conversation')
 
 mongoose
     .connect(config.dbUrl)
@@ -17,6 +18,47 @@ const io = require("socket.io")(server, { origins: "*:*" })
 
 server.listen(config.port, () => {
     console.log(`Server running on port ${config.port}`)
+})
+
+// socket.io code needs to be moved
+
+io.on("connection", (socket) => {
+    console.log("connected socket:", socket.request.connection.remoteAddress)
+    socket.on("JOIN_ROOM", async (id) => {
+        try {
+            const conversation = await Conversation.findById(id)
+            if (conversation != null) {
+                socket.join("room:" + id, () => {
+                    console.log("Socket: " + socket.request.connection.remoteAddress + " joined room:" + id)
+                })
+                return socket.local.emit("SET_CONVERSATION", conversation)
+            } else {
+                console.log("Socket: " + socket.request.connection.remoteAddress + " unsuccessfully tried to join room:" + id)
+                return socket.local.emit("SET_ERROR", "No conversation with such Id")
+            }
+        } catch (error) {
+            console.log("error: ", error)
+            socket.local.emit("SET_ERROR", "Server error: " + error)
+        }
+    })
+    socket.on("LEAVE_ROOM", (id) => {
+        socket.leave("room:" + id, () => {
+            console.log("Socket: " + socket.request.connection.remoteAddress + " left room:" + id)
+        })
+    })
+    socket.on("POST_COMMENT", async (id, comment) => {
+        try {
+            console.log("Socket: " + socket.request.connection.remoteAddress + " posted comment: " + comment + " in room:" + id)
+            const conversation = await Conversation.findOneAndUpdate({ _id: id }, { $push: { messages: comment } })
+            io.in("room:" + id).emit("UPDATE_CONVERSATION", comment)
+        } catch (error) {
+            console.log("error: " + error)
+            socket.local.emit("SET_ERROR", "Server error: " + error)
+        }
+    })
+    socket.on('disconnect', function () {
+        io.emit('user disconnected');
+    });
 })
 
 
